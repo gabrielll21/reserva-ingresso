bf8fca7ed63d591a82bf769d342c80e9fb248a00
Gabriel Fernandes <gabrielpaesfernandes0@gmail.com>
2025-04-11 20:03:09 -0300

    Add files via upload
    Signed-off-by: Gabriel Fernandes <143446301+gabrielll21@users.noreply.github.com>

--- re-click/src/main/java/com/re_click/entity/Usuario.java
+++ re-click/src/main/java/com/re_click/entity/Usuario.java
@@ -1,4 +1,11 @@
 package com.re_click.entity;
 
+import jakarta.persistence.Entity;
+
+//@Entity
 public class Usuario {
+    private int id;
+    private String nome;
+    private String cpf;
+    
 }
