e1744ec4092d4b19a91f45f005513f190319f688
Gabriel Fernandes <143446301+gabrielll21@users.noreply.github.com>
2025-06-15 12:06:48 -0300

    Delete reserva-ingresso-main directory
    Signed-off-by: Gabriel Fernandes <143446301+gabrielll21@users.noreply.github.com>

--- reserva-ingresso-main/re-click/src/main/java/com/re_click/model/StatusPagamento.java
+++
@@ -1,7 +0,0 @@
-package com.re_click.model;
-
-public enum StatusPagamento {
-    PENDENTE,
-    CONFIRMADO,
-    RECUSADO
-}
--- reserva-ingresso-main/re-click/src/main/java/com/re_click/model/TipoConta.java
+++
@@ -1,6 +0,0 @@
-package com.re_click.model;
-
-public enum TipoConta {
-    USUARIO,
-    VENDEDOR
-}
