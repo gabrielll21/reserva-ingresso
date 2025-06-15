40db9ba85051460397d8d6e9d96ee934371f269b
Gabriel Fernandes <143446301+gabrielll21@users.noreply.github.com>
2025-06-15 12:06:48 -0300

    Delete reserva-ingresso-main directory
    Signed-off-by: Gabriel Fernandes <143446301+gabrielll21@users.noreply.github.com>

--- reserva-ingresso-main/re-click/src/main/java/com/re_click/model/StatusEvento.java
+++
@@ -1,7 +0,0 @@
-package com.re_click.model;
-
-public enum StatusEvento {
-    PENDENTE,
-    APROVADO,
-    RECUSADO
-}
--- reserva-ingresso-main/re-click/src/main/java/com/re_click/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.re_click.repository;
-
-import com.re_click.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
